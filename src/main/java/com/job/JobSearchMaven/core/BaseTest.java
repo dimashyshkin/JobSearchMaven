b28e916ccc664e4611dbf19077258ffeda8ebdbc
dimashyshkin <dimashyshkin@hotmail.com>
2015-01-09 13:05:12 -0600

    Added browser parameter

--- src/main/java/com/job/JobSearchMaven/core/BaseTest.java
+++ src/main/java/com/job/JobSearchMaven/core/BaseTest.java
@@ -1,16 +1,27 @@
 package com.job.JobSearchMaven.core;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 public abstract class BaseTest {
 	protected WebDriver driver;
 	
 	@BeforeTest(alwaysRun = true)
-	public void setUp() {
-		driver = new FirefoxDriver();
+	@Parameters({"browser"})
+	public void setUp(String browser) {
+		if (browser.equals("firefox")) {
+			driver = new FirefoxDriver();
+		} else if (browser.equals("ie")) {
+			driver = new InternetExplorerDriver();
+		} else if (browser.equals("chrome")) {
+			driver = new ChromeDriver();
+		} else {driver = new FirefoxDriver();} 
+		
 	}
 	
 	@AfterTest(alwaysRun = true)
