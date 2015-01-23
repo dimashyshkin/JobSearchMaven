584cdfe8859e75650812cf378d1040043161d9c8
dimashyshkin <dimashyshkin@hotmail.com>
2015-01-23 14:39:03 -0600

    Component and Wait finished

--- src/main/java/com/job/JobSearchMaven/core/web/Component.java
+++ src/main/java/com/job/JobSearchMaven/core/web/Component.java
@@ -1,10 +1,12 @@
 package com.job.JobSearchMaven.core.web;
 
-public abstract class Component {
+public abstract class Component<T extends Component<T>> {
 
 	public abstract boolean isAvailable();
 	
-	public void waitUntilAvailable(){
-		
+	public T waitUntilAvailable(){
+		return new Wait<T>().forComponent((T) this).toBeAvailable();
 	}
+	
+	
 }
--- src/main/java/com/job/JobSearchMaven/core/web/Wait.java
+++ src/main/java/com/job/JobSearchMaven/core/web/Wait.java
@@ -1,11 +1,40 @@
 package com.job.JobSearchMaven.core.web;
 
-public class Wait {
+import org.openqa.selenium.TimeoutException;
+
+public class Wait<T extends Component<T>> {
 
 	private static final int DEFAULT_TIMEOUT = 30000;
 	private static final int DEFAULT_RETRY_DELAY = 500;
+	private T component;
 	
-	public Wait(){
-		
+	public Wait(){}
+	
+	public Wait<T> forComponent(T component){
+		this.component = component;
+		return this;
+	}
+	
+	public T toBeAvailable(){
+		int timePassed = 0;
+		while (timePassed < DEFAULT_TIMEOUT) {
+			if (this.component.isAvailable()) {
+				return this.component;
+			}
+			timePassed = timePassed + delay();
+		}
+		if (!this.component.isAvailable()) {
+			throw new TimeoutException("Timed out after " + DEFAULT_TIMEOUT + "ms. waiting for component to be available.");
+		}
+		return this.component;
+	}
+	
+	private int delay(){
+		try {
+			Thread.sleep(DEFAULT_RETRY_DELAY);
+			return DEFAULT_RETRY_DELAY;
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
