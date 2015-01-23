a5cde1d0ed67eb52024ec0c5e475742472fb52c7
dimashyshkin <dimashyshkin@hotmail.com>
2015-01-23 14:19:21 -0600

    wait constructor

---
+++ src/main/java/com/job/JobSearchMaven/core/web/Wait.java
@@ -0,0 +1,11 @@
+package com.job.JobSearchMaven.core.web;
+
+public class Wait {
+
+	private static final int DEFAULT_TIMEOUT = 30000;
+	private static final int DEFAULT_RETRY_DELAY = 500;
+	
+	public Wait(){
+		
+	}
+}
